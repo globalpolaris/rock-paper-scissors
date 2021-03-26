b9166bd80811b192d03caba8115f8941ed0b8225
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-03-26 14:04:39 +0700

    fixed broken timer

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-let timer = 5;
-
+var timer;
 
 let userScore = 0;
 let compScore = 0;
@@ -8,6 +7,7 @@ const comp_score = document.getElementById("comp-score");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const ann = document.getElementById("ann");
 
 function computerMove() {
   let choice = ["r", "s", "p"];
@@ -25,89 +25,131 @@ function compWin() {
   comp_score.innerHTML = compScore;
 }
 
-function click() {
-
+function change(choice) {
+  if (choice == "r") return "Rock";
+  else if (choice == "s") return "Scissors";
+  else if (choice == "p") return "Paper";
 }
 
 function game(userChoice) {
   let comp = computerMove();
+  const translate = change(userChoice);
+  const translate2 = change(comp);
   console.log(userChoice + comp);
   switch (userChoice + comp) {
     case "rr":
       console.log("DRAW!");
+      ann.innerHTML = `${translate} is the same as ${translate2}. DRAW!!`;
       break;
     case "rs":
       console.log("User Win!");
+      ann.innerHTML = `${translate} beats ${translate2}!`;
       userWin();
       break;
     case "rp":
       console.log("Computer Win!");
+      ann.innerHTML = `${translate} lose to ${translate2}!`;
       compWin();
       break;
     case "sr":
       console.log("Computer Win!");
+      ann.innerHTML = `${translate} lose to ${translate2}!`;
       compWin();
       break;
     case "ss":
       console.log("DRAW!");
+      ann.innerHTML = `${translate} is the same as ${translate2}. DRAW!!`;
       break;
     case "sp":
       console.log("User Win!");
+      ann.innerHTML = `${translate} beats ${translate2}!`;
       compWin();
       break;
     case "pr":
       console.log("User Win!");
+      ann.innerHTML = `${translate} beats ${translate2}!`;
       userWin();
       break;
     case "pp":
       console.log("DRAW!");
+      ann.innerHTML = `${translate} is the same as ${translate2}. DRAW!!`;
       break;
     case "ps":
       console.log("Computer Win!");
+      ann.innerHTML = `${translate} lose to ${translate2}!`;
       compWin();
       break;
     default:
-      // alert("You dont pick anything!");
+      ann.innerHTML = `Did you not pick anything?`;
       console.log("You dont Pick anythin!");
   }
 }
 
 let user_choice = null;
-getUser =(userChoice) => userChoice;
+getUser = (userChoice) => userChoice;
 
 function playAgain() {
-  timer = 5;
+  ann.innerHTML = "Pick your choice";
   main();
 }
-
-const pa = document.getElementById("btn-play");
-function main() {
+var Timer;
+var running = true;
+function startTimer() {
   timer = 5;
   let chc;
-  const Timer = setInterval(function () {
-    if (timer <= 0) {
-      clearInterval(Timer);
-      game(chc);
-      document.getElementById("timers").innerHTML = "Time's Up!";
-      pa.style.cssText = "display:flex;"
-      pa.addEventListener("click", function() {
-        pa.style.cssText = "display:none"
-        playAgain();
-      });
-    } else if(timer > 0) {
+  running = true;
+  Timer = setInterval(function () {
+    if (timer >= 0) {
       rock.addEventListener("click", function () {
         chc = getUser("r");
       });
       paper.addEventListener("click", function () {
-        chc =getUser("p");
+        chc = getUser("p");
       });
       scissors.addEventListener("click", function () {
-        chc =getUser("s");
+        chc = getUser("s");
       });
       document.getElementById("timers").innerHTML = timer + " second(s)";
+      timer -= 1;
+    } else {
+      while(running){
+        running = false;
+        game(chc);
+        pa.style.cssText = "display:flex;";
+        pa.addEventListener("click", function () {
+          clearInterval(Timer);
+          pa.style.cssText = "display:none";
+          playAgain();
+        });
+      }
     }
-    timer -= 1;
   }, 1000);
 }
 
+const pa = document.getElementById("btn-play");
+function main() {
+  startTimer();
+}
+
 main();
+
+// console.log("Timer:" + timer);
+// rock.addEventListener("click", function () {
+//   chc = getUser("r");
+// });
+// paper.addEventListener("click", function () {
+//   chc =getUser("p");
+// });
+// scissors.addEventListener("click", function () {
+//   chc =getUser("s");
+// });
+// document.getElementById("timers").innerHTML = timer + " second(s)";
+
+// timer = 5;
+//       game(chc);
+//       document.getElementById("timers").innerHTML = "Time's Up!";
+//       pa.style.cssText = "display:flex;"
+//       pa.addEventListener("click", function() {
+//         pa.style.cssText = "display:none"
+//         playAgain();
+//       });
